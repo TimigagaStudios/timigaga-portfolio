ce1b8eda45d1f4c6fa34668237d001ea96655b1b
TimigagaStudios <timigaga.official@gmail.com>
2026-03-15 16:42:33 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
               <img
                 src="/images/timigaga-logo-icon.png"
                 alt="Timigaga Studios icon"
-                className="h-14 w-auto object-contain"
+                className="h-25 w-auto object-contain"
               />
 
               <div className="flex flex-col leading-none">
